cb372609e5fd4cce0be2e6cf4706732939e2c871
ships22 <sshipsss@gmail.com>
2020-05-30 04:08:33 +0200

    Footer completed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { FooterComponent } from './components/footer/footer.component';
 
 const MaterialComponents = [MatButtonModule];
 
@@ -27,6 +28,7 @@ const MaterialComponents = [MatButtonModule];
     TestComponent,
     HomeComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule, MaterialComponents, CarouselModule],
   providers: [AuthenticationService, AdminService, ClientService],
