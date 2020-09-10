b0eba4e61cd122939fb4ef277c67301f3ee2c148
ships22 <sshipsss@gmail.com>
2020-09-11 01:12:49 +0200

    Filter added to the products

--- client/src/app/components/all-products/all-products.component.ts
+++ client/src/app/components/all-products/all-products.component.ts
@@ -4,6 +4,7 @@ import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/models/products/product';
 import { take } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-all-products',
@@ -15,12 +16,15 @@ export class AllProductsComponent implements OnInit {
   selectedCat: '';
   p: number = 1;
   collection: any[] = [];
+  filteredProduct: any[] = [];
 
-  constructor(private productService: ProductService, private router: Router,) {
+  constructor(private productService: ProductService, private router: Router,
+    private categoryService: CategoryService) {
    }
 
   ngOnInit(): void {
     this.getAllProducts();
+    this.getAllCategory();
   }
 
   getAllProducts() {
@@ -29,26 +33,32 @@ export class AllProductsComponent implements OnInit {
     .subscribe(response => {
       console.log('test products call :' , response);
       this.collection = response;
-      
+
       this.collection.forEach(product => {
-        
-        
+
+
         product.stock.forEach(stock => {
           product['inStock'] =+ stock.quantite;
         });
-      })
+      });
+      this.filteredProduct = this.collection;
+    })
+  }
+  getAllCategory() {
+    this.categoryService.getAllCategory()
+    .subscribe(data => {
+      this.categories = data;
     })
   }
 
   filter(selectedCat) {
-    // let filteredDvds = this.dvds.filter((dvd) => dvd.cat === selectedCat);
-    // if (selectedCat) {
-    //   return this.dvdDisplay = filteredDvds;
-    // } else if (selectedCat.length === 0) {
-    //   return this.dvdDisplay = this.dvds;
-    // }
     console.log('test cat :' , selectedCat);
-    
+    if(selectedCat) {
+      this.collection = this.filteredProduct.filter(product => product.categories[0].name == selectedCat);
+    } else {
+      this.collection = this.filteredProduct;
+    }
+
   }
   selectProduct(productRef) {
     this.router.navigate(["/product_select", productRef]);
